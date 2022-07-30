c3485a1f237bfb9d65935739b6f3c14f83283948
Kal <46393179+kalvin807@users.noreply.github.com>
2022-07-31 01:24:22 +0900

    Create 443-StringCompression.py

---
+++ 443-StringCompression.py
@@ -0,0 +1,20 @@
+class Solution:
+    def compress(self, chars: List[str]) -> int:
+        write_at = 0
+        read_at = 0
+        while read_at < len(chars):
+            c, cnt = chars[read_at], 1
+            while read_at + 1 < len(chars) and chars[read_at] == chars[read_at + 1]:
+                cnt += 1
+                read_at += 1
+            
+            chars[write_at] = c
+            if cnt > 1:
+                for n_c in str(cnt):
+                    write_at += 1
+                    chars[write_at] = n_c
+            read_at += 1
+            write_at += 1
+        return write_at
+# Time complexity = O(n)
+# Space complexity = O(1)
