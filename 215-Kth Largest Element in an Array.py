630edbc762b99209955201e5602ebf62595a3c7d
Kal <46393179+kalvin807@users.noreply.github.com>
2022-08-01 13:16:29 +0900

    Create 215-Kth Largest Element in an Array.py

---
+++ 215-Kth Largest Element in an Array.py
@@ -0,0 +1,14 @@
+import heapq
+class Solution:
+    def findKthLargest(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        heap = nums[:k]
+        heapq.heapify(heap)  # create a min-heap whose size is k 
+        for num in nums[k:]:
+            if num > heap[0]:
+                heapq.heapreplace(heap, num)
+        return heap[0]
