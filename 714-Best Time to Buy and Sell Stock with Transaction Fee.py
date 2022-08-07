a377a680373ba2dc109ea801d8afec94114e52bf
Kal <46393179+kalvin807@users.noreply.github.com>
2022-08-07 13:54:33 +0900

    Create 714-Best Time to Buy and Sell Stock with Transaction Fee.py

---
+++ 714-Best Time to Buy and Sell Stock with Transaction Fee.py
@@ -0,0 +1,14 @@
+class Solution:
+    def maxProfit(self, prices: List[int], fee: int) -> int:
+        cur_hold, cur_not_hold = -float("inf"), 0
+        
+        for p in prices:
+            prev_hold, prev_not_hold = cur_hold, cur_not_hold
+            # Everyday if u are holding stock that means
+            
+            # You Diamond hand or you bought today stock
+            cur_hold = max(prev_hold, prev_not_hold - p)
+            # Cash is king or you close position
+            cur_not_hold = max(prev_not_hold, prev_hold + p - fee)
+        
+        return cur_not_hold if cur_not_hold >= 0 else 0 
