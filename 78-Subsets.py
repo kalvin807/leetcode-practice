f40f987f1947decdfcde25f55cb590df6cb73d96
Kal <46393179+kalvin807@users.noreply.github.com>
2022-08-13 22:24:01 +0900

    Create 78-Subsets.py

---
+++ 78-Subsets.py
@@ -0,0 +1,10 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        ans = set()
+        self.dfs(nums,[], ans)
+        return ans
+    
+    def dfs(self, ns, path, ans):
+        ans.add(tuple(sorted(path)))
+        for i, n in enumerate(ns):
+            self.dfs(ns[i + 1:], path + [n], ans)
